e327e710162aba3352a621e7cf3670619739417a
tuberkulbek <a.burmaganov5@gmail.com>
2023-03-29 18:17:31 +0600

    wpp

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import Do from "./components/Do/Do";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import { Routes, Route } from "react-router-dom";
+import logo from "./assets/images/wpp.png"
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -17,7 +18,8 @@ function App() {
         <Route path={"/do"} element={<Do />} />
         <Route path={"/contact"} element={<Contact />} />
       </Routes>
-    </>
+      <a href="https://api.whatsapp.com/send?phone=77079520550" rel="noreferrer" target="_blank"><img src={logo} alt="whatsapp" className="logo_wpp"/></a>
+    </div>
   );
 }
 
